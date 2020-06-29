0e4183cf9a09d2744e0d29218b0b58b218c5cb65
Satyam Kumar <satyamthedeveloper@gmail.com>
2020-06-29 09:17:42 +0530

    Structuring Week 2 tasks

--- microservices/BookAppApiBooks/src/main/resources/data.sql
+++
@@ -1,3 +0,0 @@
-insert into Book(id, title, author, price) values(1, 'Java', 'Martin', 100.00);
-insert into Book(id, title, author, price) values(2, 'JavaScript', 'Mark', 200.00);
-insert into Book(id, title, author, price) values(3, 'JQuery', 'Mary', 349.90);
